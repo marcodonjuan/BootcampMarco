d48e2d422fe2c25be6ba014a0ad789f12a477b31
marcodonjuan <91994328+marcodonjuan@users.noreply.github.com>
2021-10-21 16:37:16 -0500

    Add files via upload

--- pom/pages/Todaypage.js
+++ pom/pages/Todaypage.js
@@ -23,5 +23,39 @@ class TodayPage {
         this.deleteTaskButton =Selector ('[class="icon_menu_item__content"]').withExactText('Delete task')
         this.deleteConfirmation = Selector ('[class="ist_button ist_button_red"]')
     }
+    async TaskMultiple (){
+        var count = 0;
+        while (count <10){
+            await t
+            .typeText(this.taskInputField, 'New '+count, { speed: 0.01 })
+            .click(this.addTaskButton)
+            count++; 
+        }
+    }
+    async TaskTomorrow (){
+        await t
+        .wait(2000)
+        .click(this.plusButton)
+        .typeText(this.taskInputField, 'New', { speed: 0.01 })
+        .click(this.taskDueSelector)
+        .wait(3000)
+        .click(this.taskDueTomorrowSelector.withText('Tomorrow'))
+        .wait(2000)
+        .click(this.addTaskButton)
+        .click(this.inboxButton)
+        .wait(1000)
+    }
+    async NewProjectFavorite(){
+        await t 
+        .wait(2000)
+        .click(this.addProjectIcon)
+        .typeText(this.projectNameField, 'Bootcamp')
+        .click(this.projectColorButton)
+        .wait(1500)
+        .click(this.projectColorButton.withText('Berry Red'))
+        .click(this.favoriteSwitch)
+        .wait(2000)
+        .click(this.addProjectButton, {speed:0.01}) 
+    }
 }
 export default new TodayPage
